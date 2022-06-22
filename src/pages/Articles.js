7b54b3409002457535e4ba6875a618e80ee00f92
Joseph Manning <jgmanning3@yahoo.com>
2022-06-22 16:44:45 -0700

    Setup to render data from server

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const Articles = () => {
-  return <h1>Articles</h1>;
+  const [articles, setArticles] = useState(window && window.preloadedArticles);
+
+  useEffect(() => {
+    if (window && !window.preloadedArticles) {
+      console.log("No preloaded articles found, loading from server");
+      fetch("/api/articles")
+        .then((res) => res.json())
+        .then((data) => setArticles(data));
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Articles</h1>
+      {articles &&
+        articles.map((article) => (
+          <div key={article.title}>
+            <h3>{article.title}</h3>
+            <p>by {article.author}</p>
+          </div>
+        ))}
+    </>
+  );
 };
